80fb4f704d1f6a9c69c73ad3903e259dceb6a9d2
Brandon Zhang <brandonzhang53@gmail.com>
2020-01-09 17:28:46 -0800

    add stuff to point and graph

--- src/Graph.h
+++ src/Graph.h
@@ -1,8 +1,23 @@
 #pragma once
 
 #include <bits/stdc++.h>
+#include "Point.h"
 using namespace std;
 
 struct graph {
-  vector<set<int, >> adj;
-}
+  vector<pt> points;
+  struct pt_cmp {
+    pt o;
+    const vector<pt>& v;
+    pt_cmp(const pt& base, const vector<pt>& pts): o(base), v(pts) {}
+    bool operator()(const int a, const int b) const {
+      return cp(v[a], v[b], o) < 0;
+    }
+  };
+  vector<set<int, pt_cmp>> adj;
+  void add_vertex(pt i) {
+    assert(i.i == points.size());
+    points.push_back(i);
+    adj.emplace_back(i, points);
+  }
+};
--- src/Point.h
+++ src/Point.h
@@ -5,4 +5,16 @@ using namespace std;
 
 struct pt {
   int x, y, i;
+  pt operator+(const pt& o) const {
+    return {x+o.x, y+o.y, -1};
+  }
+  pt operator-(const pt& o) const {
+    return {x-o.x, y-o.y, -1};
+  }
+  friend int64_t cp(const pt& a, const pt& b) {
+    return (int64_t)a.x*b.y - (int64_t)a.y*b.x;
+  }
+  friend int64_t cp(const pt& a, const pt& b, const pt& o) {
+    return cp(a-o, b-o);
+  }
 };
