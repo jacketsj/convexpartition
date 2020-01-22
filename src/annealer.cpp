30cfd62e12cdea9ad8aae329db621bff9253aa03
UBC Student <zhangbr@thetis.students.cs.ubc.ca>
2020-01-22 14:12:03 -0800

    parallel thread pool annealer

--- src/annealer.cpp
+++ src/annealer.cpp
@@ -3,24 +3,19 @@ using namespace std;
 #include "Annealer.h"
 #include "Graph.h"
 
-void run(const string& filename) {
-  graph g;
-  g.read("../triangulations/"+filename+".tri");
-  int m = g.get_edge_num();
-  annealer ann(g);
-  ann.anneal();
-  cerr << filename << " started with " << m << " edges and finished with " << g.get_edge_num() <<endl;
-  g.write("../min_from_triangulation/"+filename+".out");
-}
-
 int main() {
   ifstream in("../base_names.txt");
   string filename = "euro-night-0010000";
-  vector<thread> work;
   while(in >> filename) {
-    work.emplace_back(run, filename);
-  }
-  for (auto& t : work) {
-    t.join();
+    if (filename < "uniform-0090000-2") continue;
+    graph g;
+    g.read("../triangulations/"+filename+".tri");
+//    g.print_matlab();
+    int m = g.get_edge_num();
+    annealer ann(g);
+    ann.anneal();
+    cerr << filename << " started with " << m << " edges and finished with " << g.get_edge_num() <<endl;
+    g.write("../min_from_triangulation/"+filename+".out");
+//    g.print_matlab();
   }
 }
