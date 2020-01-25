b1fb489dfab0829e57062788acf7a68b26c7f831
zhengdw <zhengdw@alumni.ubc.ca>
2020-01-24 22:45:38 -0800

    Super change, fixed bugs and added 'good edge set'.

--- src/Annealer.h
+++ src/Annealer.h
@@ -17,7 +17,7 @@ struct annealer {
   annealer(graph& _g) : rng(0xF), //rng(chrono::high_resolution_clock::now().time_since_epoch().count()), 
                         g(_g), dis(0.0L, 1.0L) {
     // n log^2 n should be enough whp to visit all edges
-    MAXT = 11*50*g.n*log2(g.n)*log2(g.n);  // ~ 12 hrs
+    MAXT = 11*10*g.n*log2(g.n)*log2(g.n);  // ~ 12 hrs
     MAXIT = MAXT+3*g.n*log(g.n)+10000000;
     temperature = 1;
   }
@@ -30,9 +30,9 @@ struct annealer {
     //temperature = max(0.L, 0.3 * (1 - 4*sqr((ld) it / MAXT - 0.5L)));
   }
 
-  pair<int,int> bad_sample_halfedge() { 
+  pair<int,int> sample_good_edge() { 
     // This is slow! we sample via pbds order stat tree
-    return *g.inner_edges.find_by_order(rng()%g.inner_edges.size());
+    return *g.good_edges.find_by_order(rng()%g.good_edges.size());
   }
 
 
@@ -41,40 +41,32 @@ struct annealer {
     // 1. sample random edge
     // 2. flip with probability temperature
     // 3. remove if possible with probability 1-temperature.
-    pair<int, int> halfedge = bad_sample_halfedge();
-    int a = halfedge.first;
-    int b = halfedge.second;
+    pair<int, int> edge = sample_good_edge();
+    int a = edge.first;
+    int b = edge.second;
+    assert(g.adj[a].count(b)); assert(g.adj[b].count(a));
     if (rng()%2) swap(a,b);
     if (dis(rng) >= temperature) { // removal probability
-      if (g.can_remove(a,b)) {
-        g.remove_edge(a,b);
+      if (g.can_remove(a, b)) {
+        g.remove_edge(a, b, 1);
         success++;
       }
     }
     else {
-      /* actions to try:
-      g.flip(a,b);
-      g.rot_cw(a,b);
-      g.rot_ccw(a,b);
-      g.rot_cw(b,a);
-      g.rot_ccw(b,a);
-
-      */
-      if (g.flip(a,b)) success++;
+      if (g.flip(a, b, 1)) success++;
       else if (rng()%2) {
-        if (g.rot_cw(a,b)) success++;
-        else if (g.rot_ccw(a,b)) success++;
+        if (g.rot_cw(a, b, 1)) success++;
+        else if (g.rot_ccw(a, b, 1)) success++;
       }
       else {
-        if (g.rot_ccw(a,b)) success++;
-        else if (g.rot_cw(a,b)) success++;
+        if (g.rot_ccw(a, b, 1)) success++;
+        else if (g.rot_cw(a, b, 1)) success++;
       }
     }
-    //++it;
   }
   
   void anneal() {
-    static const int64_t PRINT_ITER = 1e7;
+    static const int64_t PRINT_ITER = 1e6;
     cerr << "ANNEALING FOR " << MAXIT << " iterations" <<endl;
     for(it=1;it<=MAXIT;it++) { // run some extra steps for safety
       if (it % PRINT_ITER == 0) cerr << "ANNEALING ITERATION " << it << " TEMP = " << temperature << " SUCCESS% = " << (ld) success / it << endl;
