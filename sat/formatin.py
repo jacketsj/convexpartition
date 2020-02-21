221a3a7eadbe5db4f2c23bb7ca36b8d1df686788
j <jacketsj@gmail.com>
2020-02-21 14:51:18 -0800

    Clean formatin.py

--- sat/formatin.py
+++ sat/formatin.py
@@ -1,3 +1,7 @@
+'''
+This file will create a ZIP of the best solutions.
+'''
+
 # Load an instance
 import os
 import io
@@ -36,54 +40,33 @@ def readSolution(instance):
     solution.delete_double_edges()
     return solution
 
-#solutions = BestSolutionSet()
 og = sys.stdin
 vis = Visualizer()
 checker = SolutionChecker()
 solutions = BestSolutionSet()
 names = open("../base_names.txt")
-#for instance in idb:
 for name in names:
     name = name[:-1]
-    # change this for more instances
+    # Here we can filter our instances. For example:
     #if len(instance) < 150 and instance.name.find("stars") == -1:
-    #if name.find("rop") != -1:
+    # Reviewing all instances gives us the following:
     if True:
-    #if name.find("rop") != -1:
         print(f"Reading solution to {name}")
+        # Solutions should be stored in ../best (this can be changed):
         f = open('../best/'+name+'.out','r')
         sys.stdin = f
         solution = readSolution(name)
         solutions.add(solution)
         sys.stdin = og
+        # The following can be used to check our solutions, and print to PDF.
+        # However, it is too slow to run on all instances, and will not run on mona-lisa.
         #status = checker(instance=instance, solution=solution)
         #print(status.is_feasible())
         #print(status.get_message())
         #print(status.get_objective_value())
         #vis.visualize_solution(solution=solution,instance=instance,path="pdf/"+instance.name+".pdf")
 
-#instance_loc = "rop0000541"
-#instance = idb[instance_loc]
-#
-#name = "rop0000541"
-#sys.stdin = open('../min_from_triangulation/'+name+'.out','r')
-#solution = readSolution(name)
-#
-#checker = SolutionChecker()
-#status = checker(instance=instance, solution=solution)
-#print(status.is_feasible())
-#print(status.get_message())
-#print(status.get_objective_value())
-#vis = Visualizer()
-#print("visualizing")
-#vis.visualize_solution(solution=solution, instance=instance) # opens plot if possible
-#print("visualized")
-#vis.visualize_solution(solution=solution, instance=instance, path="fig_of_sat_on_" + instance_loc + ".pdf") # writes plot to file
-
 # write solutions into zip
 print("Creating zip. This can take some time...")
 with SolutionZipWriter("continuous-restart.zip") as zipper:
     zipper.add_solutions(solutions)
-
-#print("You can now upload 'my_first_upload.zip' on",
-      #"https://cgshop.ibr.cs.tu-bs.de/competition/cg-shop-2020/")
